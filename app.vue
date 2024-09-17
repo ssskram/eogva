fa7029ac40912892e3563c48656ea00a962a3058
ssskram <paul.marks@geovisual-analytics.com>
2024-09-17 13:25:05 -0700

    It's just a big ol empty map, man

--- app.vue
+++ app.vue
@@ -1,5 +1,67 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="map-container">
+    <div ref="mapContainer" class="map-child"></div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted, nextTick, onBeforeUnmount } from "vue";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const mapContainer = ref<HTMLDivElement | null>(null);
+let map: mapboxgl.Map | null = null;
+
+const forceMapResize = () => {
+  if (map) {
+    requestAnimationFrame(() => {
+      map?.resize();
+    });
+  }
+};
+
+const initializeMap = async () => {
+  await nextTick();
+
+  if (mapContainer.value && !map) {
+    map = new mapboxgl.Map({
+      container: mapContainer.value,
+      style: "mapbox://styles/mapbox/satellite-v9",
+      center: [-121.6555, 36.6777],
+      zoom: 10,
+    });
+
+    map.on("load", () => {
+      forceMapResize();
+    });
+  }
+};
+
+onMounted(async () => {
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoic3Nza3JhbSIsImEiOiJja3Nvd25veHEwMDVyMm5wbm92Zm5jbGp1In0.ADb1kC_9vj4D9psqDa6dqA";
+  await initializeMap();
+  window.addEventListener("resize", forceMapResize);
+});
+
+onBeforeUnmount(() => {
+  if (map) {
+    map.remove();
+  }
+  window.removeEventListener("resize", forceMapResize);
+});
+</script>
+
+<style scoped>
+.map-container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+
+.map-child {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["@/assets/css/main.css"],
   devtools: { enabled: true },
   routeRules: {
-    // prerender index route by default
-    '/': { prerender: true },
+    "/": { prerender: true },
   },
+  compatibilityDate: "2024-09-17",
 });
