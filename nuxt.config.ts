c6b0d6b06fda42eaa57b5336dfcdad4ce2b3ceac
ssskram <paul.marks@geovisual-analytics.com>
2024-09-19 18:24:03 -0700

    Add head title description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@ export default defineNuxtConfig({
   build: {
     transpile: ["@pinia/nuxt"],
   },
+  app: {
+    head: {
+      title: 'GVA Earth Observation',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Geovisual Analytics Earth Observation' }
+      ]
+    }
+  },
   compatibilityDate: "2024-09-17",
   css: ["@/assets/css/main.css"],
   devtools: { enabled: true },
